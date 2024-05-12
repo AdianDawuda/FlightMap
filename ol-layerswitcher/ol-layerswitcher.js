231e6bf81593aef6f62132bb821a7194a1b24147
Nimrod2022 <nimrodmandelakibet@gmail.com>
2024-05-12 15:42:58 +0200

    variables update

--- ol-layerswitcher/ol-layerswitcher.js
+++ ol-layerswitcher/ol-layerswitcher.js
@@ -7,16 +7,16 @@
 Control = 'default' in Control ? Control['default'] : Control;
 LayerGroup = 'default' in LayerGroup ? LayerGroup['default'] : LayerGroup;
 
-var classCallCheck = function (instance, Constructor) {
+let classCallCheck = function (instance, Constructor) {
   if (!(instance instanceof Constructor)) {
     throw new TypeError("Cannot call a class as a function");
   }
 };
 
-var createClass = function () {
+let createClass = function () {
   function defineProperties(target, props) {
-    for (var i = 0; i < props.length; i++) {
-      var descriptor = props[i];
+    for (let i = 0; i < props.length; i++) {
+      let descriptor = props[i];
       descriptor.enumerable = descriptor.enumerable || false;
       descriptor.configurable = true;
       if ("value" in descriptor) descriptor.writable = true;
@@ -37,12 +37,12 @@ var createClass = function () {
 
 
 
-var get = function get(object, property, receiver) {
+let get = function get(object, property, receiver) {
   if (object === null) object = Function.prototype;
-  var desc = Object.getOwnPropertyDescriptor(object, property);
+  let desc = Object.getOwnPropertyDescriptor(object, property);
 
   if (desc === undefined) {
-    var parent = Object.getPrototypeOf(object);
+    let parent = Object.getPrototypeOf(object);
 
     if (parent === null) {
       return undefined;
@@ -52,7 +52,7 @@ var get = function get(object, property, receiver) {
   } else if ("value" in desc) {
     return desc.value;
   } else {
-    var getter = desc.get;
+    let getter = desc.get;
 
     if (getter === undefined) {
       return undefined;
@@ -62,7 +62,7 @@ var get = function get(object, property, receiver) {
   }
 };
 
-var inherits = function (subClass, superClass) {
+let inherits = function (subClass, superClass) {
   if (typeof superClass !== "function" && superClass !== null) {
     throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);
   }
@@ -88,7 +88,7 @@ var inherits = function (subClass, superClass) {
 
 
 
-var possibleConstructorReturn = function (self, call) {
+let possibleConstructorReturn = function (self, call) {
   if (!self) {
     throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
   }
@@ -99,7 +99,7 @@ var possibleConstructorReturn = function (self, call) {
 /**
  * @protected
  */
-var CSS_PREFIX = 'layer-switcher-';
+let CSS_PREFIX = 'layer-switcher-';
 /**
  * OpenLayers LayerSwitcher Control, displays a list of layers and groups
  * associated with a map which have a `title` property.
@@ -120,7 +120,7 @@ var CSS_PREFIX = 'layer-switcher-';
  *
  * Specify a `title` for a Layer by adding a `title` property to it's options object:
  * ```javascript
- * var lyr = new ol.layer.Tile({
+ * let lyr = new ol.layer.Tile({
  *   // Specify a title property which will be displayed by the layer switcher
  *   title: 'OpenStreetMap',
  *   visible: true,
@@ -130,7 +130,7 @@ var CSS_PREFIX = 'layer-switcher-';
  *
  * Alternatively the properties can be set via the `set` method after a layer has been created:
  * ```javascript
- * var lyr = new ol.layer.Tile({
+ * let lyr = new ol.layer.Tile({
  *   visible: true,
  *   source: new ol.source.OSM()
  * })
@@ -140,7 +140,7 @@ var CSS_PREFIX = 'layer-switcher-';
  *
  * To create a LayerSwitcher and add it to a map, create a new instance then pass it to the map's [`addControl` method](https://openlayers.org/en/latest/apidoc/module-ol_PluggableMap-PluggableMap.html#addControl).
  * ```javascript
- * var layerSwitcher = new LayerSwitcher({
+ * let layerSwitcher = new LayerSwitcher({
  *   reverse: true,
  *   groupSelectStyle: 'group'
  * });
@@ -152,27 +152,27 @@ var CSS_PREFIX = 'layer-switcher-';
  * @param opt_options LayerSwitcher options, see  [LayerSwitcher Options](#options) and [RenderOptions](#renderoptions) which LayerSwitcher `Options` extends for more details.
  */
 
-var LayerSwitcher = function (_Control) {
+let LayerSwitcher = function (_Control) {
     inherits(LayerSwitcher, _Control);
 
     function LayerSwitcher(opt_options) {
         classCallCheck(this, LayerSwitcher);
 
-        var options = Object.assign({}, opt_options);
+        let options = Object.assign({}, opt_options);
         // TODO Next: Rename to showButtonTitle
-        var tipLabel = options.tipLabel ? options.tipLabel : 'Legend';
+        let tipLabel = options.tipLabel ? options.tipLabel : 'Legend';
         // TODO Next: Rename to hideButtonTitle
-        var collapseTipLabel = options.collapseTipLabel ? options.collapseTipLabel : 'Collapse legend';
-        var element = document.createElement('div');
+        let collapseTipLabel = options.collapseTipLabel ? options.collapseTipLabel : 'Collapse legend';
+        let element = document.createElement('div');
 
-        var _this = possibleConstructorReturn(this, (LayerSwitcher.__proto__ || Object.getPrototypeOf(LayerSwitcher)).call(this, { element: element, target: options.target }));
+        let _this = possibleConstructorReturn(this, (LayerSwitcher.__proto__ || Object.getPrototypeOf(LayerSwitcher)).call(this, { element: element, target: options.target }));
 
         _this.activationMode = options.activationMode || 'mouseover';
         _this.startActive = options.startActive === true;
         // TODO Next: Rename to showButtonContent
-        var label = options.label !== undefined ? options.label : '';
+        let label = options.label !== undefined ? options.label : '';
         // TODO Next: Rename to hideButtonContent
-        var collapseLabel = options.collapseLabel !== undefined ? options.collapseLabel : '\xBB';
+        let collapseLabel = options.collapseLabel !== undefined ? options.collapseLabel : '\xBB';
         _this.groupSelectStyle = LayerSwitcher.getGroupSelectStyle(options.groupSelectStyle);
         _this.reverse = options.reverse !== false;
         _this.mapListeners = [];
@@ -182,7 +182,7 @@ var LayerSwitcher = function (_Control) {
         }
         _this.shownClassName = 'shown';
         element.className = _this.hiddenClassName;
-        var button = document.createElement('button');
+        let button = document.createElement('button');
         button.setAttribute('title', tipLabel);
         button.setAttribute('aria-label', tipLabel);
         element.appendChild(button);
@@ -202,7 +202,7 @@ var LayerSwitcher = function (_Control) {
                 button.setAttribute('aria-label', collapseTipLabel);
             }
             button.onclick = function (e) {
-                var evt = e || window.event;
+                let evt = e || window.event;
                 if (_this.element.classList.contains(_this.shownClassName)) {
                     _this.hidePanel();
                     button.textContent = label;
@@ -221,7 +221,7 @@ var LayerSwitcher = function (_Control) {
                 _this.showPanel();
             };
             button.onclick = function (e) {
-                var evt = e || window.event;
+                let evt = e || window.event;
                 _this.showPanel();
                 evt.preventDefault();
             };
@@ -242,10 +242,10 @@ var LayerSwitcher = function (_Control) {
     createClass(LayerSwitcher, [{
         key: 'setMap',
         value: function setMap(map) {
-            var _this2 = this;
+            let _this2 = this;
 
             // Clean up listeners associated with the previous map
-            for (var i = 0; i < this.mapListeners.length; i++) {
+            for (let i = 0; i < this.mapListeners.length; i++) {
                 ol_Observable.unByKey(this.mapListeners[i]);
             }
             this.mapListeners.length = 0;
@@ -312,7 +312,7 @@ var LayerSwitcher = function (_Control) {
         key: 'renderPanel',
         value: function renderPanel(map, panel, options) {
             // Create the event.
-            var render_event = new Event('render');
+            let render_event = new Event('render');
             // Dispatch the event.
             panel.dispatchEvent(render_event);
             options = options || {};
@@ -334,14 +334,14 @@ var LayerSwitcher = function (_Control) {
                 // Set child indetermiate state based on their parent's visibility
                 LayerSwitcher.setChildVisibility(map);
             }
-            var ul = document.createElement('ul');
+            let ul = document.createElement('ul');
             panel.appendChild(ul);
             // passing two map arguments instead of lyr as we're passing the map as the root of the layers tree
             LayerSwitcher.renderLayers_(map, map, ul, options, function render(_changedLyr) {
                 LayerSwitcher.renderPanel(map, panel, options);
             });
             // Create the event.
-            var rendercomplete_event = new Event('rendercomplete');
+            let rendercomplete_event = new Event('rendercomplete');
             // Dispatch the event.
             panel.dispatchEvent(rendercomplete_event);
         }
@@ -354,7 +354,7 @@ var LayerSwitcher = function (_Control) {
         key: 'isBaseGroup',
         value: function isBaseGroup(grp) {
             if (grp instanceof LayerGroup) {
-                var lyrs = grp.getLayers().getArray();
+                let lyrs = grp.getLayers().getArray();
                 return lyrs.length && lyrs[0].get('type') === 'base';
             } else {
                 return false;
@@ -364,14 +364,14 @@ var LayerSwitcher = function (_Control) {
         key: 'setGroupVisibility',
         value: function setGroupVisibility(map) {
             // Get a list of groups, with the deepest first
-            var groups = LayerSwitcher.getGroupsAndLayers(map, function (l) {
+            let groups = LayerSwitcher.getGroupsAndLayers(map, function (l) {
                 return l instanceof LayerGroup && !l.get('combine') && !LayerSwitcher.isBaseGroup(l);
             }).reverse();
             // console.log(groups.map(g => g.get('title')));
             groups.forEach(function (grp) {
                 // TODO Can we use getLayersArray, is it public in the esm build?
-                var descendantVisibility = grp.getLayersArray().map(function (l) {
-                    var state = l.getVisible();
+                let descendantVisibility = grp.getLayersArray().map(function (l) {
+                    let state = l.getVisible();
                     // console.log('>', l.get('title'), state);
                     return state;
                 });
@@ -396,14 +396,14 @@ var LayerSwitcher = function (_Control) {
         key: 'setChildVisibility',
         value: function setChildVisibility(map) {
             // console.log('setChildVisibility');
-            var groups = LayerSwitcher.getGroupsAndLayers(map, function (l) {
+            let groups = LayerSwitcher.getGroupsAndLayers(map, function (l) {
                 return l instanceof LayerGroup && !l.get('combine') && !LayerSwitcher.isBaseGroup(l);
             });
             groups.forEach(function (grp) {
-                var group = grp;
+                let group = grp;
                 // console.log(group.get('title'));
-                var groupVisible = group.getVisible();
-                var groupIndeterminate = group.get('indeterminate');
+                let groupVisible = group.getVisible();
+                let groupIndeterminate = group.get('indeterminate');
                 group.getLayers().getArray().forEach(function (l) {
                     l.set('indeterminate', false);
                     if ((!groupVisible || groupIndeterminate) && l.getVisible()) {
@@ -422,7 +422,7 @@ var LayerSwitcher = function (_Control) {
     }, {
         key: 'ensureTopVisibleBaseLayerShown',
         value: function ensureTopVisibleBaseLayerShown(map, groupSelectStyle) {
-            var lastVisibleBaseLyr = void 0;
+            let lastVisibleBaseLyr = void 0;
             LayerSwitcher.forEachRecursive(map, function (lyr, _idx, _arr) {
                 if (lyr.get('type') === 'base' && lyr.getVisible()) {
                     lastVisibleBaseLyr = lyr;
@@ -440,7 +440,7 @@ var LayerSwitcher = function (_Control) {
     }, {
         key: 'getGroupsAndLayers',
         value: function getGroupsAndLayers(grp, filterFn) {
-            var layers = [];
+            let layers = [];
             filterFn = filterFn || function (_lyr, _idx, _arr) {
                 return true;
             };
@@ -496,12 +496,12 @@ var LayerSwitcher = function (_Control) {
     }, {
         key: 'renderLayer_',
         value: function renderLayer_(map, lyr, idx, options, render) {
-            var li = document.createElement('li');
-            var lyrTitle = lyr.get('title');
-            var checkboxId = LayerSwitcher.uuid();
-            var label = document.createElement('label');
+            let li = document.createElement('li');
+            let lyrTitle = lyr.get('title');
+            let checkboxId = LayerSwitcher.uuid();
+            let label = document.createElement('label');
             if (lyr instanceof LayerGroup && !lyr.get('combine')) {
-                var isBaseGroup = LayerSwitcher.isBaseGroup(lyr);
+                let isBaseGroup = LayerSwitcher.isBaseGroup(lyr);
                 li.classList.add('group');
                 if (isBaseGroup) {
                     li.classList.add(CSS_PREFIX + 'base-group');
@@ -510,22 +510,22 @@ var LayerSwitcher = function (_Control) {
                 if (lyr.get('fold')) {
                     li.classList.add(CSS_PREFIX + 'fold');
                     li.classList.add(CSS_PREFIX + lyr.get('fold'));
-                    var btn = document.createElement('button');
+                    let btn = document.createElement('button');
                     btn.onclick = function (e) {
-                        var evt = e || window.event;
+                        let evt = e || window.event;
                         LayerSwitcher.toggleFold_(lyr, li);
                         evt.preventDefault();
                     };
                     li.appendChild(btn);
                 }
                 if (!isBaseGroup && options.groupSelectStyle != 'none') {
-                    var input = document.createElement('input');
+                    let input = document.createElement('input');
                     input.type = 'checkbox';
                     input.id = checkboxId;
                     input.checked = lyr.getVisible();
                     input.indeterminate = lyr.get('indeterminate');
                     input.onchange = function (e) {
-                        var target = e.target;
+                        let target = e.target;
                         LayerSwitcher.setVisible_(map, lyr, target.checked, options.groupSelectStyle);
                         render(lyr);
                     };
@@ -534,12 +534,12 @@ var LayerSwitcher = function (_Control) {
                 }
                 label.innerHTML = lyrTitle;
                 li.appendChild(label);
-                var ul = document.createElement('ul');
+                let ul = document.createElement('ul');
                 li.appendChild(ul);
                 LayerSwitcher.renderLayers_(map, lyr, ul, options, render);
             } else {
                 li.className = 'layer';
-                var _input = document.createElement('input');
+                let _input = document.createElement('input');
                 if (lyr.get('type') === 'base') {
                     _input.type = 'radio';
                     _input.name = 'base';
@@ -550,14 +550,14 @@ var LayerSwitcher = function (_Control) {
                 _input.checked = lyr.get('visible');
                 _input.indeterminate = lyr.get('indeterminate');
                 _input.onchange = function (e) {
-                    var target = e.target;
+                    let target = e.target;
                     LayerSwitcher.setVisible_(map, lyr, target.checked, options.groupSelectStyle);
                     render(lyr);
                 };
                 li.appendChild(_input);
                 label.htmlFor = checkboxId;
                 label.innerHTML = lyrTitle;
-                var rsl = map.getView().getResolution();
+                let rsl = map.getView().getResolution();
                 if (rsl > lyr.getMaxResolution() || rsl < lyr.getMinResolution()) {
                     label.className += ' disabled';
                 }
@@ -577,9 +577,9 @@ var LayerSwitcher = function (_Control) {
     }, {
         key: 'renderLayers_',
         value: function renderLayers_(map, lyr, elm, options, render) {
-            var lyrs = lyr.getLayers().getArray().slice();
+            let lyrs = lyr.getLayers().getArray().slice();
             if (options.reverse) lyrs = lyrs.reverse();
-            for (var i = 0, l; i < lyrs.length; i++) {
+            for (let i = 0, l; i < lyrs.length; i++) {
                 l = lyrs[i];
                 if (l.get('title')) {
                     elm.appendChild(LayerSwitcher.renderLayer_(map, l, i, options, render));
@@ -614,7 +614,7 @@ var LayerSwitcher = function (_Control) {
         key: 'uuid',
         value: function uuid() {
             return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-                var r = Math.random() * 16 | 0,
+                let r = Math.random() * 16 | 0,
                     v = c == 'x' ? r : r & 0x3 | 0x8;
                 return v.toString(16);
             });
@@ -630,7 +630,7 @@ var LayerSwitcher = function (_Control) {
         key: 'enableTouchScroll_',
         value: function enableTouchScroll_(elm) {
             if (LayerSwitcher.isTouchDevice_()) {
-                var scrollStartPos = 0;
+                let scrollStartPos = 0;
                 elm.addEventListener('touchstart', function (event) {
                     scrollStartPos = this.scrollTop + event.touches[0].pageY;
                 }, false);
